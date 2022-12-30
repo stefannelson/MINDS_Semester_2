336d6a0260bcfce84f6dbe62e328d2c4a8cd17ec
Stefan Nelson <stefanwnelson@gmail.com>
2022-12-30 08:59:33 -0600

    Update Module 4

---
+++ data/build_csv.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+
+def to_csv():
+    db = sqlite3.connect('../data/cdeschools.sqlite')
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    for table_name in tables:
+        table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+        table.to_csv("../data/"+table_name + '.csv', index_label='index')
+    cursor.close()
+    db.close()
